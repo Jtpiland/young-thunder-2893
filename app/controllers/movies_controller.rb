7bbe2266a50337f02aba713cb33c410793c43cce
Jacob Piland <piland.jacob@gmail.com>
2021-07-19 12:56:54 -0500

    add user story three tests, form and methods- not functional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,21 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def actors
+    @movie = Movie.find(params[:id])
+    @actors = @movie.actors
+  end
+
+  def update
+    movie = Movie.find(params[:movie_id])
+    if params[:search]
+      actor = Actor.find(params[:search])
+
+      movie.actors << actor
+    end
+
+    redirect_to "/movies/#{movie.id}"
+  end
+
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Movies Show Page' do
     @shrek = @universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
     @harrison = @raiders.actors.create!(name: 'Harrison Ford', age: 78)
     @alfred = @raiders.actors.create!(name: 'Alfred Molina', age: 68)
+    @denholm = Actor.create!(name: 'Denholm Elliott', age:70)
 
     visit "/movies/#{@raiders.id}"
   end
@@ -24,7 +25,7 @@ RSpec.describe 'Movies Show Page' do
   end
 
   it 'can list all the actors in that movie in alphabetical order' do
-    
+
     expect(page).to have_content(@harrison.name)
     expect(page).to have_content(@alfred.name)
     expect(@alfred.name).to appear_before(@harrison.name)
@@ -34,5 +35,28 @@ RSpec.describe 'Movies Show Page' do
 
   expect(page).to have_content('Average Age of Actors')
   expect(@raiders.actors.avg_age).to eq(73)
+  end
+
+#   As a user,
+# When I visit a movie show page,
+# I do not see any actors listed that are not part of the movie
+# And I see a form to add an actor to this movie
+# When I fill in the form with the name of an actor that exists in the database
+# And I click submit
+# Then I am redirected back to that movie's show page
+# And I see the actor's name is now listed
+  it 'has a form to add an existing actor to a movie' do
+
+    expect(page).to have_content('Add Actor To Movie')
+    expect(find('form')).to have_content('Name')
+  end
+
+  it 'redirects back to the movies show page and lists the new actor' do
+    fill_in 'Name', with: 'Denholm Elliott'
+
+    click_button 'Add Actor'
+
+    expect(current_path).to eq("/movies/#{@raiders.id}")
+    expect(page).to have_content('Denholm Elliott')
   end
 end
